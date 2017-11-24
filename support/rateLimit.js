a4690e35481aebc8288ddec5482bbdb170dcd1e9
tanliyuan <tanliyuan516867305@163.com>
2017-11-24 16:10:32 +0800

    修改限流ip获取

--- support/rateLimit.js
+++ support/rateLimit.js
@@ -8,6 +8,11 @@ module.exports = new RateLimit({
     client: redisClient
   }),
   windowMs: 1*60*1000, // 15 minutes 
-  max: 2, // limit each IP to 100 requests per windowMs 
-  delayMs: 0 // disable delaying - full speed until the max limit is reached 
+  max: 10, // limit each IP to 100 requests per windowMs 
+  delayMs: 0 ,// disable delaying - full speed until the max limit is reached ,
+  keyGenerator: function(req) {
+      let real_ip = req.get("X-Real-IP") || req.get("X-Forwarded-For") || req.ip;  
+      return real_ip;
+  },
+  message: '目前限流 QPS: 10req/ip/min'
 });
