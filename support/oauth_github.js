055d9e35ff35516d73e3a311d1ad2e6d9f794130
tanliyuan <tanliyuan516867305@163.com>
2017-11-25 16:35:07 +0800

    update

--- support/oauth_github.js
+++ support/oauth_github.js
@@ -4,11 +4,11 @@ var uuidV5 = require('uuid/v5');
 
 
 var githubAuth = new ClientOAuth2({
-    clientId: config.clientId,
-    clientSecret: config.clientSecret,
+    clientId: config.client_id,
+    clientSecret: config.client_secret,
     accessTokenUri: 'https://github.com/login/oauth/access_token',
     authorizationUri: 'https://github.com/login/oauth/authorize',
-    redirectUri: config.redirectUri,
+    redirectUri: config.redirect_rri,
     scopes: ['user', 'gist'],
     state: uuidV5(config.domain, uuidV5.URL)
 });
